7251b0f5fdd749b201485fe33f6831bcac036cc3
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-23 02:19:58 +0500

    chore: remove out and add to gitignore

--- out/_next/static/79-ZAwi6E135AUjfawOXe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-8d499edba61e1479.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/posts/[slug]":["static/chunks/pages/posts/[slug]-23b3e41e89bb8242.js"],sortedPages:["/","/_app","/_error","/posts/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
