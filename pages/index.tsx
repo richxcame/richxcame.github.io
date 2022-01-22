7389df413df4530363fa1c0325a44b8c29eca703
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-22 17:55:26 +0500

    seo: import seo in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import { NextSeo } from 'next-seo';
 
 const Home: NextPage = () => {
-	return <></>;
+	return (
+		<>
+			<NextSeo title='richxcame' description='Blog of Baygeldi Cholukov' />
+		</>
+	);
 };
 
 export default Home;
