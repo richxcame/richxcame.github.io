7e23a3a7865c2143699b1bb20ccd4545653bf204
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-22 17:26:21 +0500

    style: call custom vars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../src/theme';
+import { CssBaseline } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
