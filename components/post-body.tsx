407dd0b99698c7fc6c50804d576ccbf1ec1e5351
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-23 00:10:11 +0500

    chore: create post-body component

---
+++ components/post-body.tsx
@@ -0,0 +1,13 @@
+type Props = {
+	content: string;
+};
+
+const PostBody = ({ content }: Props) => {
+	return (
+		<div>
+			<div dangerouslySetInnerHTML={{ __html: content }} />
+		</div>
+	);
+};
+
+export default PostBody;
