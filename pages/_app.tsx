1536aeea2f7a335d6c5b466d67cc19a1d508debb
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-22 17:30:02 +0500

    style: container to full page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../src/theme';
-import { CssBaseline } from '@mui/material';
+import { Container, CssBaseline } from '@mui/material';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
@@ -10,7 +9,9 @@ function MyApp({ Component, pageProps }: AppProps) {
 			<ThemeProvider theme={theme}>
 				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 				<CssBaseline />
-				<Component {...pageProps} />
+				<Container>
+					<Component {...pageProps} />
+				</Container>
 			</ThemeProvider>
 		</>
 	);
