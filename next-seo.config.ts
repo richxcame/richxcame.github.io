413dcec972c76af832e2a51df549594e3834d94a
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-22 17:53:08 +0500

    seo: improve seo options

---
+++ next-seo.config.ts
@@ -0,0 +1,15 @@
+const SEO = {
+	openGraph: {
+		type: 'website',
+		locale: 'tk',
+		url: 'https://richxcame.github.io/',
+		site_name: 'Baygeldi Cholukov',
+	},
+	twitter: {
+		handle: '@Baygeldi12',
+		site: '@Baygeldi12',
+	},
+	titleTemplate: '%s | Baygeldi Cholukov',
+};
+
+export default SEO;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../src/theme';
 import { Container, CssBaseline } from '@mui/material';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
 				<CssBaseline />
 				<Container>
+					<DefaultSeo {...SEO} />
 					<Component {...pageProps} />
 				</Container>
 			</ThemeProvider>
