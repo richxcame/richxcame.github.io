178c8d2bda5a324b7e332f94dd88c6ef1509c9db
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-23 02:13:42 +0500

    test: rename repo name in next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-	assetPrefix: isProd ? '/' : '',
+	assetPrefix: isProd ? '/richxcame.github.io' : '',
 	reactStrictMode: true,
 	webpack: config => {
 		config.module.rules.push({
